27bd6d2dfe3126d6474e6f8b4d348db1bdfd67a0
Gakuraa <gabrieliwakura924@gmail.com>
2024-03-13 11:25:48 -0300

    Kotlin Jetpack Compose Initial Studies

---
+++ app/src/main/java/br/com/fiap/delivery/MainActivity.kt
@@ -0,0 +1,51 @@
+package br.com.fiap.delivery
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.fiap.delivery.ui.theme.DeliveryTheme
+import java.sql.Ref
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DeliveryTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting() {
+    Column (modifier = Modifier.padding(32.dp).fillMaxWidth()){
+        OutlinedTextField(modifier = Modifier.fillMaxWidth(),
+            value = "",
+            onValueChange = {},
+            placeholder = {
+                Text(text = "O que você deseja buscar hoje?")
+            },
+            trailingIcon = {
+                Icon(painter = painterResource(id =  R.drawable.baseline_search_24), contentDescription = "botão de busca")
+            }
+        )
+    }
+}
